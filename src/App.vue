ea76ee3b5208c22fa3cb1ed39d1c83abf8ad6006
Rodrigo Maria <rodrigo@rodrigomaria.com.br>
2024-04-23 18:09:17 -0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home RODRIGO</RouterLink>
+        <RouterLink to="/">Home RODRIGO TESTE</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
